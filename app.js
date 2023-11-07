0690cc8766a0c6b97a74a7ede618fa86f2e0a27b
Hamza Abd El-Baset <hamza.abdelbaset.elgebaly@gmail.com>
2023-11-07 11:42:36 +0200

    static file is added for doucmentation

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app = express()
 
 //Middlewares
 app.use(express.json())
+app.use(express.static('public'))
+
+// Mount the API documentation at the root path
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/documentation/index.html')
+  })
 
 //Routes
 app.use('/api/auth', require('./routes/authRoute'))
